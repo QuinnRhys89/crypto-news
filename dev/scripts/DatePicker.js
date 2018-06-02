1ce21456bc45e315764d774a3dc6cf35a792a9aa
Quinn Anderson <quinnrhys89@gmail.com>
2018-06-02 07:57:47 -0400

    first commit

---
+++ dev/scripts/DatePicker.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+
+class DatePicker extends React.Component {
+    render () {
+        return (
+            <div>
+                <h1>Coinsquare</h1>
+            </div>
+        )
+    }
+}
+
+export default DatePicker;
